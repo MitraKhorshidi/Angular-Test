2cc717b3db7588f373159cdd2b68b8cc6eb216d7
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-09-06 15:51:56 +0200

    adding service

---
+++ src/app/services/my-first-service.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyFirstServiceService {
+
+
+  messages : Array<any>=[];
+
+  constructor() { 
+    this.init();
+  }
+  init():void{
+    this.insert({name:'Mitra',email:'mitra@mail.com',message:'message'});
+    this.insert({name:'Hossain',email:'mitra@mail.com',message:'message'});
+    this.insert({name:'Roya',email:'mitra@mail.com',message:'message'});
+  }
+
+  insert(message:{name:string , email:string , message:string}):void{
+    this.messages.push(message);
+  }
+
+  getAll():any[]{
+    return this.messages;
+  }
+
+  delete(index:number):void{
+    this.messages.splice(index,1);
+  }
+}
